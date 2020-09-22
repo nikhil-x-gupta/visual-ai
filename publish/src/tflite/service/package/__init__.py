84d88cca2489cab5ec984704c55a6ec1d39e2c49
Sanjeev Gupta <sanjeev@ibm.com>
2020-09-22 14:13:08 -0700

    refactor

--- publish/src/tflite/service/package/__init__.py
+++ publish/src/tflite/service/package/__init__.py
@@ -1,12 +1,10 @@
-#from .tflite import Config
-#from .tflite import Detector
-#from .tflite import OpenCV
-#from .tflite import VideoStream
+#
+# __init__.py
+#
 
 from .config import Config
 from .detector import Detector
 from .openCV import OpenCV
-
 from .videoStream import VideoStream
 from .videoSource import VideoSource
 from .videoSourceProcessor import VideoSourceProcessor
