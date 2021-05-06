a365ace080076b0810fe411b2a6e06b321aa27cf
Sanjeev Gupta <sanjeev@ibm.com>
2021-05-06 13:00:05 -0700

    sample video file copying to bind

--- register/node_register_app.sh
+++ register/node_register_app.sh
@@ -34,6 +34,7 @@ fn_register_with_policy() {
     echo "Registering with ..."
     echo "   node_policy_${FMWK}.json"
     echo "   user_input_app_${FMWK}.json"
+    echo ""
 
     hzn exchange node create -n $HZN_EXCHANGE_NODE_AUTH
     hzn register --policy=node_policy_${FMWK}.json --input-file user_input_app_${FMWK}.json
@@ -89,7 +90,7 @@ else
 fi
 
 if [ -z "$VIDEO_FILES" ]; then
-    export APP_VIDEO_FILES="-"
+    APP_VIDEO_FILES="-"
 else
     files=(${VIDEO_FILES//,/ })
     for file in "${files[@]}"; do
@@ -99,7 +100,7 @@ else
 	    exit 1
 	fi
     done
-    export APP_VIDEO_FILES="$VIDEO_FILES"
+    # export later after copying files in bind directory"
 fi
 
 if [ -z "$RTSPS" ]; then export APP_RTSPS="-"; else export APP_RTSPS="$RTSPS"; fi
@@ -134,25 +135,55 @@ elif [ "$FMWK" = "tflite" ] || [ "$FMWK" = "vino" ] || [ "$FMWK" = "mvi" ] || [ 
 
     fn_chk_env
 
+    export APP_MODEL_FMWK="$FMWK"
+
     if [ "$FMWK" = "tflite" ] || [ "$FMWK" = "mvi" ] || [ "$FMWK" = "pth_cpu" ] || [ "$FMWK" = "pth_gpu" ]; then
 	if [ ! -z $MI_MODEL ]; then 
 	    if [ -f $MI_MODEL ]; then 
-		MODEL_DIR="$APP_BIND_HORIZON_DIR/ai/mi/model/$FMWK"
-		echo "Creating directory $MODEL_DIR"
-		mkdir -p $MODEL_DIR
-		if [ -d $MODEL_DIR ]; then 
+		APP_MODEL_SUB_DIR="ml/model/$FMWK"
+		APP_MODEL_DIR="$APP_BIND_HORIZON_DIR/$APP_MODEL_SUB_DIR"
+
+		export APP_MODEL_SUB_DIR="$APP_MODEL_SUB_DIR"
+		export APP_MODEL_DIR="$APP_MODEL_DIR"
+
+		echo ""
+		echo "Using $APP_BIND_HORIZON_DIR as bind volume for containers"
+		echo ""
+		echo "Creating directory $APP_MODEL_DIR"
+
+		mkdir -p $APP_MODEL_DIR
+		if [ -d $APP_MODEL_DIR ]; then 
 		    export APP_MI_MODEL="$(basename $MI_MODEL)"
 		    if [ "$FMWK" = "mvi" ]; then
-			echo "Copying $MI_MODEL $MODEL_DIR/mi_mvi_model.zip"
-			cp $MI_MODEL $MODEL_DIR/mi_mvi_model.zip
+			echo "Copying $MI_MODEL to $APP_MODEL_DIR/mi_mvi_model.zip"
+			cp $MI_MODEL $APP_MODEL_DIR/mi_mvi_model.zip
 		    else
-			echo "Copying $MI_MODEL to $MODEL_DIR"
-			cp $MI_MODEL "$MODEL_DIR/default-$APP_MI_MODEL"
+			echo "Copying $MI_MODEL to $APP_MODEL_DIR"
+			cp $MI_MODEL "$APP_MODEL_DIR/default-$APP_MI_MODEL"
 		    fi
 		else
-		    echo "Failed creating directoy $MODEL_DIR"
+		    echo "Failed creating directoy $APP_MODEL_DIR"
 		    echo "Provide 777 privilege to $APP_BIND_HORIZON_DIR directory so that model files can be copied."
 		    exit 1
+		fi
+
+		if [ ! -z "$VIDEO_FILES" ]; then
+		    APP_SAMPLE_VIDEO_DIR="$APP_BIND_HORIZON_DIR/sample/video"
+		    mkdir -p $APP_SAMPLE_VIDEO_DIR
+		    files=(${VIDEO_FILES//,/ })
+		    APP_SAMPLE_VIDEO_FILES=""
+		    echo ""
+		    for file in "${files[@]}"; do
+			SAMPLE_FILE="$(basename $file)"
+			echo "Copying $file to $APP_SAMPLE_VIDEO_DIR/$SAMPLE_FILE"
+			cp $file "$APP_SAMPLE_VIDEO_DIR/$SAMPLE_FILE"
+			if [ "$APP_SAMPLE_VIDEO_FILES"  == "" ]; then
+			    APP_SAMPLE_VIDEO_FILES="$APP_SAMPLE_VIDEO_DIR/$SAMPLE_FILE"
+			else
+			    APP_SAMPLE_VIDEO_FILES="$APP_SAMPLE_VIDEO_FILES,$APP_SAMPLE_VIDEO_DIR/$SAMPLE_FILE"
+			fi 
+		    done
+		    export APP_VIDEO_FILES="$APP_SAMPLE_VIDEO_FILES"
 		fi
 	    else
 		echo "Model file $MI_MODEL does not exist."
@@ -176,12 +207,13 @@ else
     exit 1
 fi
 
+echo ""
 echo "Application ML Model Framework: $FMWK"
-if [ "$APP_CAMERAS" = "-" ]; then echo "Video Source: Cameras: None"; else echo "Video Source: Cameras: $APP_CAMERAS"; fi
-if [ "$APP_VIDEO_FILES" = "-" ]; then echo "Video Source: Files: None"; else echo "Video Source: Files: $APP_VIDEO_FILES"; fi
-if [ "$APP_RTSPS" = "-" ]; then echo "Video Source: RTSP: None"; else echo "Video Source: RTSP Streams: $APP_RTSPS"; fi
-echo "View column: $APP_VIEW_COLUMNS"
-echo "Application bind directory: $APP_BIND_HORIZON_DIR"
+if [ "$APP_CAMERAS" = "-" ]; then echo "Video Source: Camera:           None"; else echo "Video Source: Camera:           $APP_CAMERAS"; fi
+if [ "$APP_VIDEO_FILES" = "-" ]; then echo "Video Source: File:             None"; else echo "Video Source: File:             $APP_VIDEO_FILES"; fi
+if [ "$APP_RTSPS" = "-" ]; then echo "Video Source: RTSP:             None"; else echo "Video Source: RTSP Stream:      $APP_RTSPS"; fi
+echo "View column:                    $APP_VIEW_COLUMNS"
+echo ""
 
 fn_register_with_policy
 
