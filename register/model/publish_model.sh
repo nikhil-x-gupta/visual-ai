0bfcc90a25e7ce5aaec3ebdaef652ec398927e14
Sanjeev Gupta <sanjeev@ibm.com>
2022-05-10 21:37:38 -0700

    publish_model ENV made optional

--- register/model/publish_model.sh
+++ register/model/publish_model.sh
@@ -8,9 +8,9 @@ MODEL_ROOT_DIR=../model
 usage() {                      
   echo "Usage: $0 -e -k -m -f -v "
   echo "where "
-  echo "   -e Environemnt veriables full file path"
-  echo "   -k Framework tflite | pth "
-  echo "   -v ML model version"
+  echo "   -e (Optional) Environemnt veriables full file path"
+  echo "   -k (Required) Framework tflite | pth "
+  echo "   -v (Required) ML model version"
 # Not needed, they are built from k and v . Expect model-file-name and publish defintion file in fixed format
 #  echo "   -f ML model file "
 #  echo "   -m Model definition file full path"
@@ -46,63 +46,68 @@ shift $((OPTIND - 1))
 
 if [ -z $ENVVAR ]; then
     echo ""
-    echo "Must provide ENV vars file. Needs HZN_ORG_ID, EDGE_OWNER, EDGE_DEPLOY"
+    echo "Using HZN_ORG_ID, EDGE_OWNER, EDGE_DEPLOY from current ENV"
+    echo ""
+else
+    echo ""
+    echo "Using provided HZN_ORG_ID, EDGE_OWNER, EDGE_DEPLOY from provided ENV file"
+    echo ""
+    . $ENVVAR
+fi 
+
+if [ -z $MODEL_FMWK ]; then
+    echo ""
+    echo "Missing -k "
+    echo "Must provide one framework  tflite | pth_cpu "
     echo ""
     usage
     exit 1
 else 
-    if [ -z $MODEL_FMWK ]; then
+    if [ -z $MODEL_VERSION ]; then
 	echo ""
-	echo "Missing -k "
-	echo "Must provide one framework  tflite | pth_cpu "
+	echo "Missing -v "
+	echo "Must provide model version "
 	echo ""
 	usage
 	exit 1
     else 
-	if [ -z $MODEL_VERSION ]; then
-	    echo ""
-	    echo "Missing -v "
-	    echo "Must provide model version "
-	    echo ""
-	    usage
-	    exit 1
-	else 
-	    if [ -d "$MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION" ]; then
-		MODEL_FILE="$MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION/$MODEL_FMWK-model-$MODEL_VERSION-mms.zip"
-		if [ ! -f $MODEL_FILE ]; then
+	if [ -d "$MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION" ]; then
+	    MODEL_FILE="$MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION/$MODEL_FMWK-model-$MODEL_VERSION-mms.zip"
+	    if [ ! -f $MODEL_FILE ]; then
+		echo ""
+		echo "Must provide model file $MODEL_FILE"
+		echo ""
+		usage
+		exit 1
+	    else
+		MODEL_DEF_FILE="$MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION/$MODEL_FMWK.model.publish.definition.json"
+		if [ ! -f $MODEL_DEF_FILE ]; then
 		    echo ""
-		    echo "Must provide model file $MODEL_FILE"
+		    echo "Must provide model definition file $MODEL_DEF_FILE"
 		    echo ""
 		    usage
 		    exit 1
 		else
-		    MODEL_DEF_FILE="$MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION/$MODEL_FMWK.model.publish.definition.json"
-		    if [ ! -f $MODEL_DEF_FILE ]; then
-			echo ""
-			echo "Must provide model definition file $MODEL_DEF_FILE"
-			echo ""
-			usage
-			exit 1
-		    else
-			echo "Publish $MODEL_FILE using $MODEL_DEF_FILE"
-			. $ENVVAR
-			envsubst < $MODEL_DEF_FILE | jq
-			read -p "Continue? (Y/N) " yn
-			case $yn in
-			    [Yy]* ) ;;
-			    [Nn]* ) exit;;
-			esac
-			envsubst < $MODEL_DEF_FILE | hzn mms object publish -m- -f $MODEL_FILE
-		    fi
+		    echo "Publish $MODEL_FILE using $MODEL_DEF_FILE"
+		    envsubst < $MODEL_DEF_FILE | jq
+		    echo ""
+		    echo "envsubst < $MODEL_DEF_FILE | hzn mms object publish -m- -f $MODEL_FILE"
+		    echo ""
+		    read -p "Will publish using above command and JSON. Continue? (Y/N) " yn
+		    case $yn in
+			[Yy]* ) ;;
+			[Nn]* ) exit;;
+		    esac
+		    envsubst < $MODEL_DEF_FILE | hzn mms object publish -m- -f $MODEL_FILE
 		fi
-	    else
-		echo ""
-		echo "No such directory $MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION"
-		echo "Machine learning models must be specified in this directory."
-		echo ""
-		usage
-		exit 1
 	    fi
+	else
+	    echo ""
+	    echo "No such directory $MODEL_ROOT_DIR/$MODEL_FMWK/$MODEL_VERSION"
+	    echo "Machine learning models must be specified in this directory."
+	    echo ""
+	    usage
+	    exit 1
 	fi
     fi
 fi
